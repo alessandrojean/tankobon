b94b91af21bbabe59b439c3b371738f966ced0d9
Andreas <andreas.everos@gmail.com>
2023-05-19 18:38:39 +0200

    Add Dockerfile to create Docker image (closes #32)

---
+++ server/src/main/kotlin/io/github/alessandrojean/tankobon/interfaces/IndexController.kt
@@ -0,0 +1,19 @@
+package io.github.alessandrojean.tankobon.interfaces
+
+import jakarta.servlet.ServletContext
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class IndexController(
+    servletContext: ServletContext,
+) {
+    private val baseUrl: String = "${servletContext.contextPath}/"
+
+    @GetMapping("/")
+    fun index(model: Model): String {
+        model.addAttribute("baseUrl", baseUrl)
+        return "index"
+    }
+}
