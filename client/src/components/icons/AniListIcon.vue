2daf897691bb18dacfd15b603bbca343c1f0c8f6
Alessandro Jean <14254807+alessandrojean@users.noreply.github.com>
2023-05-20 14:34:38 -0300

    Update the AniList icon colors.

--- client/src/components/icons/AniListIcon.vue
+++ client/src/components/icons/AniListIcon.vue
@@ -1,5 +1,8 @@
 <template>
-  <div aria-hidden="true" class="bg-[#02A9FF] flex items-center justify-center rounded-md">
-    <svg class="w-4 h-4" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="#fff"><path d="M6.361 2.943L0 21.056h4.942l1.077-3.133H11.4l1.052 3.133H22.9c.71 0 1.1-.392 1.1-1.101V17.53c0-.71-.39-1.101-1.1-1.101h-6.483V4.045c0-.71-.392-1.102-1.101-1.102h-2.422c-.71 0-1.101.392-1.101 1.102v1.064l-.758-2.166zm2.324 5.948l1.688 5.018H7.144z"/></svg>
+  <div aria-hidden="true" class="bg-[#19212D] flex items-center justify-center rounded-md">
+    <svg class="w-5 h-5" viewBox="0 0 4.233 4.233" xmlns="http://www.w3.org/2000/svg">
+      <path fill="#0af" d="M2.247.794c-.104 0-.16.057-.16.16v.155l.815 2.33h.807c.104 0 .162-.056.162-.16v-.354c0-.104-.058-.161-.162-.161h-.947V.954c0-.103-.057-.16-.161-.16z"></path>
+      <path fill="#fff" d="M1.293.794.363 3.44h.722l.158-.458h.786l.154.458h.719L1.976.794zm.114 1.602.225-.733.247.733z"></path>
+    </svg>
   </div>
 </template>
